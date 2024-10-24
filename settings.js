5b301a2f2b90dbca99e698227ea32db8f3db0243
AlichaXD <dheriap248@gmail.com>
2024-10-25 02:22:13 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,25 @@
+/*
+* Base ori Alicha Zahra X Fadlan Developer Apps Pentingg❗❗❗
+Hapus Bagian /Teks ini? Masuk Neraka Paling Bawah
+
+Script Ini Murni Bikinan Sendiri,dan tidak pasaran,Saya Hanya Sekedar Kroco Penghuni Inti Bumi
+
+Tanks To:
+- Allah Swt
+- Nabi Muhammad Saw
+- Fadlan Developer
+- Alicha Zahra [ Developer Sc ]
+- Xydlan Ganz
+- Pengguna Bot yang selalu Support
+
+* Recode by Alicha Zahra
+*/
+
+const settings = {
+  token: '7816798399:AAGQJt37Qe-r0cQDAhhSZaNqtbmaPGaRTwM'
+  urladmin: 'https://t.me/AlichaIcha',
+  adminId: '7277892050'
+  pp: 'https://telegra.ph/file/f1c355bbf7d77bc88f2e5.jpg'
+};
+
+module.exports = settings;
